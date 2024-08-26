a57def1faad30f9ed76566bd9144f152c0f7aed8
rk1942 <rishabhkarthikeya@gmail.com>
2024-08-26 12:18:12 +0530

    pointer

--- CheckoutSystem.cpp
+++ CheckoutSystem.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 #include <vector>
 #include "Item.cpp"
-using namespace std;
 
 class CheckoutSystem {
 public:
-    void add_item(const Item& item) {
-        items.push_back(item);
-    }
-
-    void display_items() const {
-        cout << "Available items:\n";
-        for (size_t i = 0; i < items.size(); ++i) {
-            const Item& item = items[i];
-            cout << i + 1 << ". " << item.get_name() << " - $" << item.get_price()
-                      << " (Stock: " << item.get_stock() << ")\n";
-        }
-    }
+    void add_item(const Item& item);
+    void display_items() const;
 
 private:
     vector<Item> items;
 };
+
+void CheckoutSystem::add_item(const Item& item) {
+    this->items.push_back(item);
+}
+
+void CheckoutSystem::display_items() const {
+    cout << "Available items:\n";
+    for (size_t i = 0; i < this->items.size(); ++i) {
+        const Item& item = this->items[i];
+        cout << i + 1 << ". " << item.get_name() << " - $" << item.get_price()
+              << " (Stock: " << item.get_stock() << ")\n";
+    }
+}
